a44add227dbdf69c88494100b8812a3765d92b07
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-07-06 16:16:46 -0400

    Replace findViewById with kotlinx.android.synthetic import
    Ah, that's much better!

--- app/src/main/java/com/jeremywsherman/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/jeremywsherman/geoquiz/QuizActivity.kt
@@ -3,19 +3,18 @@ package com.jeremywsherman.geoquiz
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
         listOf(
-            R.id.true_button to R.string.toast_correct,
-            R.id.false_button to R.string.toast_incorrect
+            true_button to R.string.toast_correct,
+            false_button to R.string.toast_incorrect
             )
-            .map { findViewById<Button>(it.first) to it.second}
             .forEach { (button, textID) ->
                 button.setOnClickListener {
                     Toast
