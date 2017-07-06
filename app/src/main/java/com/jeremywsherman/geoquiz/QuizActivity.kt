0f8e50035b37859905742f0e41fa8c647f444758
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-07-06 16:29:44 -0400

    QuizActivity: Add questions property

--- app/src/main/java/com/jeremywsherman/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/jeremywsherman/geoquiz/QuizActivity.kt
@@ -8,13 +8,23 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
+    val questions = arrayOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
         listOf(
             true_button to R.string.toast_correct,
             false_button to R.string.toast_incorrect
-            )
+        )
             .forEach { (button, textID) ->
                 button.setOnClickListener {
                     Toast
