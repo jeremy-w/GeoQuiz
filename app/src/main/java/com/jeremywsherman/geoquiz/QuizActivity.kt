85ce64b3311792b8f5bf19aaff47a6e22e540667
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-07-06 16:39:44 -0400

    QuizActivity: Tie toast and text to current question

--- app/src/main/java/com/jeremywsherman/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/jeremywsherman/geoquiz/QuizActivity.kt
@@ -16,22 +16,30 @@ class QuizActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
+    var currentQuestionIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        true_button.setOnClickListener { didAnswer(true) }
+        false_button.setOnClickListener { didAnswer(false) }
 
-        listOf(
-            true_button to R.string.toast_correct,
-            false_button to R.string.toast_incorrect
-        )
-            .forEach { (button, textID) ->
-                button.setOnClickListener {
-                    Toast
-                        .makeText(this@QuizActivity, textID, Toast.LENGTH_SHORT)
-                        .apply { setGravity(Gravity.TOP, 0, 0) }
-                        .show()
-                }
-            }
+        questionIndexDidChange()
+    }
+
+    private fun questionIndexDidChange() {
+        val question = questions[currentQuestionIndex]
+        question_text_view.setText(question.textResId)
+    }
+
+    private fun didAnswer(isTrue: Boolean) {
+        val question = questions[currentQuestionIndex]
+        val isCorrect = question.isTrue == isTrue
+        val toastTextId = if (isCorrect) R.string.toast_correct else R.string.toast_incorrect
+
+        Toast
+            .makeText(this@QuizActivity, toastTextId, Toast.LENGTH_SHORT)
+            .apply { setGravity(Gravity.TOP, 0, 0) }
+            .show()
     }
 }
